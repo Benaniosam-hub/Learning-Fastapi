fde9dc391759ce5b9d212a4923fe7829c6519f91
Benaniosam <benaniosam008@gmail.com>
2026-09-02 14:40:43 +0530

    second commit

--- lecture1.py
+++ lecture1.py
@@ -1,3 +1,10 @@
 value = 10
+tax_percent = 0.35
+tax = value * tax_percent
+price = value + tax
+print(price)
+first_name = "Benanio"
+second_name = "sam"
 
-print(value)
+print(first_name + second_name)
+
